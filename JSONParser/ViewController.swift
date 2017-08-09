e61b1cde688d4c664406b5a5c02937ad6a398cff
Rb2030 <rossmail2050@gmail.com>
2017-08-09 22:01:14 +0100

    first commit

---
+++ JSONParser/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  JSONParser
+//
+//  Created by Ross on 09/08/2017.
+//  Copyright © 2017 Braaaaap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let path = Bundle.main.path(forResource: "menu", ofType: "json") else { return }
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            
+            let data = try Data(contentsOf: url)
+            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            print(json)
+        }
+        catch {
+            print(error)
+        }
+        
+    }
+
+}
+
