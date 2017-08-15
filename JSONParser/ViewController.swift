d972289b9e5f1ea3eee11046a0a492b945b6aedf
Rb2030 <rossmail2050@gmail.com>
2017-08-15 22:56:35 +0100

    removing print statement.

--- JSONParser/ViewController.swift
+++ JSONParser/ViewController.swift
@@ -20,12 +20,12 @@ class ViewController: UIViewController {
             
             let data = try Data(contentsOf: url)
             let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
-            print(json)
+            //print(json)
             
             guard let array = json as? [Any] else { return }
             for user in array {
                 guard let userDict = user as? [String: Any] else { return }
-                guard let drinks = userDict["drinks"] as? String else { print("not an Int"); return }
+                guard let drinks = userDict["drinks"] as? String else { print("not a String"); return }
                 guard let junkFood = userDict["junk-food"] as? String else { return }
                 
                 print(drinks)
